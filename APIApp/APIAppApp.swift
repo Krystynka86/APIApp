2851714c1a756c65d2f46c31194b2719f69bdff9
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2024-01-03 20:53:45 +0100

    Initial Commit

---
+++ APIApp/APIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIAppApp.swift
+//  APIApp
+//
+//  Created by Cristina on 2024-01-03.
+//
+
+import SwiftUI
+
+@main
+struct APIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
